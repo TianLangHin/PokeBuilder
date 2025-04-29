b4b5fd79489bfc3857812f8e1c3a8a8c08a1f6e4
TianLangHin <tianlanghin@gmail.com>
2025-04-29 14:47:31 +1000

    Initial commit of XCode repository

---
+++ PokeBuilder/PokeBuilder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeBuilder
+//
+//  Created by Tian Lang Hin on 29/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeBuilder/PokeBuilder/PokeBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeBuilderApp.swift
+//  PokeBuilder
+//
+//  Created by Tian Lang Hin on 29/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokeBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
