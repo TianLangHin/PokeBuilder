dd365c0e9d4d7d084430c9afabb4b86cde295ed7
TianLangHin <tianlanghin@gmail.com>
2025-05-01 00:13:07 +1000

    First attempt at placeholder HomeView structure

--- PokeBuilder/PokeBuilder/View/HomeView.swift
+++ PokeBuilder/PokeBuilder/View/HomeView.swift
@@ -5,3 +5,33 @@
 //  Created by Dương Anh Trần on 29/4/2025.
 //
 
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            // Placeholder for background colour later
+            Color.white.ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                Text("PokéBuilder")
+                    .font(.largeTitle)
+                Button(action: {
+                    // Empty action to be completed with
+                    // navigation functionality
+                }) {
+                    Text("View Teams")
+                        .font(.title2)
+                }
+                .padding()
+                .buttonStyle(.borderedProminent)
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
