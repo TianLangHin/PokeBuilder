3cb34d689a8f71bff65bdfba5e68edb27242a4ed
TianLangHin <tianlanghin@gmail.com>
2025-05-01 00:48:15 +1000

    Added models for Pokemon and Teams (for internal data representation, not data loading)

--- PokeBuilder/PokeBuilder/Model/Pokemon.swift
+++ PokeBuilder/PokeBuilder/Model/Pokemon.swift
@@ -5,3 +5,48 @@
 //  Created by Dương Anh Trần on 29/4/2025.
 //
 
+import SwiftUI
+
+struct Team: Identifiable {
+    // To make each team identifiable, enabling its display in a List.
+    let id = UUID()
+
+    // Domain-specific logic: Each team has maximum 6 Pokémon.
+    static let maximumPokemon = 6
+
+    var pokemon: [Pokemon]
+}
+
+struct Pokemon: Identifiable {
+    // To make each Pokemon within a team identifiable,
+    // smoothing over any problems with displaying in a List.
+    let id = UUID()
+
+    // Domain-specific logic: Each Pokémon has maximum 4 moves.
+    static let maximumMoves = 4
+
+    // Each of the members below (except `stats`) are integers,
+    // which are used to lookup the PokéAPI to load full information.
+    var species: Int
+    var ability: Int
+    var item: Int
+    var moves: [Int]
+    var nature: Int
+
+    var stats: StatSpread
+}
+
+// A separate `StatSpread` struct makes the declaration of EVs
+// in each Pokemon as succinct in the code as possible.
+struct StatSpread {
+
+    // Domain-specific logic: Each Pokémon can be allocated maximum 510 EVs.
+    static let maximumAllocation = 510
+
+    var hitPoints: Int
+    var attack: Int
+    var defense: Int
+    var specialAttack: Int
+    var specialDefense: Int
+    var speed: Int
+}
