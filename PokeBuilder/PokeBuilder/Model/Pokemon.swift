69f1271be5fde61d6460829c6e6777d42b5ea19c
Payayaing <isabella.watt22@gmail.com>
2025-05-02 23:52:41 +1000

    Added skeleton for team view, and added some methods to Team and Pokemon structs

--- PokeBuilder/PokeBuilder/Model/Pokemon.swift
+++ PokeBuilder/PokeBuilder/Model/Pokemon.swift
@@ -15,6 +15,19 @@ struct Team: Identifiable {
     static let maximumPokemon = 6
 
     var pokemon: [Pokemon]
+
+    func addPokemon(pokemon: Pokemon) {
+        self.pokemon.append(pokemon)
+    }
+
+    func deletePokemon(pokemon: Pokemon) {
+        guard let index = self.pokemon.firstIndex(where: {$0.id == pokemon.id}) else {return}
+        self.pokemon.remove(at: index)
+    }
+
+    func numPokemon() -> Int {
+        return self.pokemon.count
+    }
 }
 
 struct Pokemon: Identifiable {
@@ -30,13 +43,17 @@ struct Pokemon: Identifiable {
 
     // Only the `name` is mandatory.
     // The rest of the members can be filled in one by one by the user.
-    var name: Int
+    var pokedexNum: Int
     var ability: String?
     var item: Int?
     var moves: [Int]
     var nature: Int?
 
     var stats: StatSpread
+
+    func getMoves() -> [Int] {
+        return self.moves
+    }
 }
 
 // A separate `StatSpread` struct makes the declaration of EVs
--- PokeBuilder/PokeBuilder/View/TeamView.swift
+++ PokeBuilder/PokeBuilder/View/TeamView.swift
@@ -5,3 +5,36 @@
 //  Created by Dương Anh Trần on 2/5/2025.
 //
 
+import SwiftUI
+
+struct TeamView : View {
+    @Binding var team: Team
+    @Binding var teamName: String
+
+    @ObservedObject var pokemonData: PokemonCacheViewModel
+
+    @State var addPokemonEnabled: //Needs to be fixed
+
+    var body: some View {
+        NavigationView {
+            Text(teamName)
+            List {
+                ForEach(team) { pokemon in
+                    NavigationLink(destination: PokemonView(pokemon: pokemon)) {
+                        HStack {
+                            VStack {
+                                Text("\(pokemon.pokedexNum)") //To be replaced by the pokemon name
+                                //AsyncImage(url: pokemon.sprite)
+                            }
+                            VStack {
+                                for move in pokemon.getMoves {
+                                    Text("\(move)")
+                                }
+                            }
+                        }
+                    }
+                }
+            } 
+        }
+    }
+}
